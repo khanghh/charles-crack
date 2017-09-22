220b13d45e8a24cce88940006ce396e3a39250fb
Khang Hoang <khang@Hoangs-MacBook-Pro.local>
2017-09-22 18:09:40 +0700

    initial commit

---
+++ 4.1.4/crack.sh
@@ -0,0 +1,2 @@
+
+javac -encoding UTF-8 gIbD.java -d .&& jar -uvf charles.jar com/xk72/charles/gIbD.class
---
+++ 4.1.4/gIbD.java
@@ -0,0 +1,6 @@
+package com.xk72.charles;
+public final class gIbD {
+	public static boolean xUFT() { return true; }
+	public static String PcqR() { return "Hoang Hong Khang"; }
+	public static String xUFT(String name, String key) { return null; }
+}
