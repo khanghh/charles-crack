4707469b3859fca8f9720e652e9e6621b7e27497
Hoang Hong Khang <hoanghongkhang@gmail.com>
2017-10-16 15:51:02 +0700

    add crack v4.2

---
+++ 4.2/WNzU.java
@@ -0,0 +1,6 @@
+package com.xk72.charles;
+public final class WNzU {
+	public static boolean OjEP() { return true; }
+	public static String DZZn() { return "Hoang Hong Khang"; }
+	public static String OjEP(String name, String key) { return null; }
+}
