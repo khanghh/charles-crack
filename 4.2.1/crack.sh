52ff0a94095dc6fa3c8516e2e8aef196b56d7604
Khang Hoang <khang@Khangs-iMac.local>
2017-12-18 19:01:31 +0700

    add crack 4.2.1

---
+++ 4.2.1/crack.sh
@@ -0,0 +1,2 @@
+
+javac -encoding UTF-8 oFTR.java -d .&& jar -uvf charles.jar com/xk72/charles/oFTR.class
---
+++ 4.2.1/oFTR.java
@@ -0,0 +1,6 @@
+package com.xk72.charles;
+public final class oFTR {
+	public static boolean Yuaz() { return true; }
+	public static String lktV() { return "Hoang Hong Khang"; }
+	public static String Yuaz(String name, String key) { return null; }
+}
