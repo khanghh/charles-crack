db5054ca017f9b888f5817c711112cd3221f27bf
Hoang Hong Khang <1511466@hcmut.edu.vn>
2018-08-15 15:09:28 +0700

    add crack 4.2.6

---
+++ 4.2.6/GPSz.java
@@ -0,0 +1,6 @@
+package com.xk72.charles;
+public final class GPSz {
+	public static boolean Dgmx() { return true; }
+	public static String Wmmw() { return "Hoang Hong Khang"; }
+	public static String Dgmx(String name, String key) { return null; }
+}
---
+++ 4.2.6/crack.sh
@@ -0,0 +1,2 @@
+
+javac -encoding UTF-8 GPSz.java -d .&& jar -uvf charles.jar com/xk72/charles/GPSz.class
