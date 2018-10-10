567adba8a1283774107b9eec271d3c946572a611
Hoang Hong Khang <hoanghongkhang@gmail.com>
2018-10-10 23:35:24 +0700

    add crack 4.2.7

---
+++ 4.2.7/crack.sh
@@ -0,0 +1,2 @@
+
+javac -encoding UTF-8 kKPk.java -d .&& jar -uvf charles.jar com/xk72/charles/kKPk.class
---
+++ 4.2.7/kKPk.java
@@ -0,0 +1,6 @@
+package com.xk72.charles;
+public final class  kKPk{
+	public static boolean lcJx() { return true; }
+	public static String JZlU() { return "Hoang Hong Khang"; }
+	public static String lcJx(String name, String key) { return null; }
+}
