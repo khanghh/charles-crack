d3427064f3af3e52bf6e039fb7ac03433c006d99
Hoàng Hồng Khang <hoanghongkhang@gmail.com>
2019-03-12 12:06:09 +0700

    add crack 4.2.7

---
+++ 4.2.8/qHTb.java
@@ -0,0 +1,6 @@
+package com.xk72.charles;
+public final class qHTb{
+	public static boolean DdNM() { return true; }
+	public static String gbef() { return "Hoang Hong Khang"; }
+	public static String DdNM(String name, String key) { return null; }
+}
