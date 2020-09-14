d245b584d648725edef919fa747450bf988d453a
Hoang Hong Khang <hoanghongkhang@gmail.com>
2020-09-14 21:31:58 +0700

    add crack 4.5.6

---
+++ 4.5.6/Bvcn.java
@@ -0,0 +1,8 @@
+package com.xk72.charles;
+public final class Bvcn {
+	public static boolean FwRs() { return true; }
+	public static boolean Gwbn() { return true; }
+	public static String InuZ() { return "Hoang Hong Khang"; }
+	public static String Wczh() { return "Hoang Hong Khang"; }
+	public static String FwRs(String name, String key) { return null; }
+}
