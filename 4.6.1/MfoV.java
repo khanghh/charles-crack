46577c5dff023e4be1a1ed3c8062a2af16e7cb79
Hoang Hong Khang <hoanghongkhang@gmail.com>
2021-04-29 10:31:16 +0700

    add crack 4.6.1

---
+++ 4.6.1/MfoV.java
@@ -0,0 +1,6 @@
+package com.xk72.charles;
+public final class MfoV {
+	public static boolean XdKP() { return true; }
+	public static String uQqp() { return "Hoang Hong Khang"; }
+  public static String XdKP(String name, String key) { return null; }
+}
---
+++ 4.6.1/crack.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javac -encoding UTF-8 MfoV.java -d .&& jar -uvf charles.jar com/xk72/charles/MfoV.class
